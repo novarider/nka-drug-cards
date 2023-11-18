99476405783d5ed700e0cf30b8ca9128125f3e28
Patrick Rudigier <pat.rudigier@gmail.com>
2023-11-18 09:27:59 +0000

    now using the default texlive docker container to build the project

--- .devcontainer/init-container.sh
+++ .devcontainer/init-container.sh
@@ -3,8 +3,5 @@
 sudo apt update
 sudo apt upgrade -y
 
-cd /tmp
-wget https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
-zcat < install-tl-unx.tar.gz | tar xf -
-cd install-tl-*/
-sudo perl ./install-tl --no-interaction
+tlmgr update --self
+tlmgr install pgf
